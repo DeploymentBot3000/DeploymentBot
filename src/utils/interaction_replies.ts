b0ca906b7b9018abf7015a94d08ec7de0b0ce449
DeploymentBot3000 <195394025+DeploymentBot3000@users.noreply.github.com>
2025-01-22 18:39:31 -0600

    Log interaction replies

--- src/utils/interaction_replies.ts
+++ src/utils/interaction_replies.ts
@@ -1,28 +1,41 @@
 import { AnySelectMenuInteraction, ButtonInteraction, ChatInputCommandInteraction, ModalSubmitInteraction } from "discord.js";
 import { buildErrorEmbed, buildSuccessEmbed } from "../embeds/embed.js";
+import { debug } from "./logger.js";
 
 type _SupportedInteractions = ModalSubmitInteraction | ButtonInteraction | AnySelectMenuInteraction | ChatInputCommandInteraction;
 
 export async function replyWithError(interaction: _SupportedInteractions, message: string) {
+    debug(`replyWithError: ${_truncateMessage(message)} to interaction: ${interaction.id}`);
     const embed = buildErrorEmbed().setTitle('Error').setDescription(message);
     await interaction.reply({ embeds: [embed], ephemeral: true });
     setTimeout(() => interaction.deleteReply().catch(() => { }), 45000);
 }
 
 export async function replyWithSuccess(interaction: _SupportedInteractions, message: string) {
+    debug(`replyWithSuccess: ${_truncateMessage(message)} to interaction: ${interaction.id}`);
     const embed = buildSuccessEmbed().setTitle('Success').setDescription(message);
     await interaction.reply({ embeds: [embed], ephemeral: true });
     setTimeout(() => interaction.deleteReply().catch(() => { }), 45000);
 }
 
 export async function editReplyWithError(interaction: _SupportedInteractions, message: string) {
+    debug(`editReplyWithError: ${_truncateMessage(message)} to interaction: ${interaction.id}`);
     const embed = buildErrorEmbed().setTitle('Error').setDescription(message);
     await interaction.editReply({ content: '', embeds: [embed], components: [] });
     setTimeout(() => interaction.deleteReply().catch(() => { }), 45000);
 }
 
 export async function editReplyWithSuccess(interaction: _SupportedInteractions, message: string) {
+    debug(`editReplyWithSuccess: ${_truncateMessage(message)} to interaction: ${interaction.id}`);
     const embed = buildSuccessEmbed().setTitle('Success').setDescription(message);
     await interaction.editReply({ content: '', embeds: [embed], components: [] });
     setTimeout(() => interaction.deleteReply().catch(() => { }), 45000);
+}
+
+function _truncateMessage(message: string) {
+    if (message.length > 100) {
+        return message.substring(0, 97) + "...";
+    } else {
+        return message;
+    }
 }
