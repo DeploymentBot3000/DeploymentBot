9ce28ad591ea42f42bfbe864671042b229e915bb
DeploymentBot3000 <195394025+DeploymentBot3000@users.noreply.github.com>
2025-01-25 15:18:35 -0600

    Remove last import of client
    Now that the last global reliance on client is removed, we can move the client to index.ts and not export it as a global variable.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import { Events, Interaction } from "discord.js";
+import { Client, Events, GatewayIntentBits, Interaction } from "discord.js";
 import { config } from "./config.js";
 import { secrets } from "./config/secrets_loader.js";
-import { client } from "./custom_client.js";
 import autocompleteInteraction from "./events/auto_complete_Interaction.js";
 import buttonInteraction from "./events/button_interaction.js";
 import chatInputCommandInteraction from "./events/chat_input_command_interaction.js";
@@ -19,6 +18,36 @@ if (!isEnumKey(LogLevel, config.logLevel)) {
 logger.level = LogLevel[config.logLevel];
 
 verbose(`Loaded secrets file with env: ${secrets.env}`);
+
+// Initialize the client
+const client = new Client({
+    // Invite link, this defines the permissions the bot has and what it can do.
+    // https://discord.com/developers/applications/1312896264475508839/oauth2
+    // Move Members
+    //   - To set permissions on new voice channels to allow the host to kick out non squad members.
+    // Create Instant Invite
+    //   - To allow the host to invite others to the voice channel.
+    // https://discord.com/oauth2/authorize?client_id=1312896264475508839&permissions=16777217&integration_type=0&scope=bot
+    // The bot then must be given the `Manage Channels` permission on the hot drops and strikes VC categories.
+
+    // Intents: Intents are event subscriptions that send information from the discord server to the bot.
+    // These are often needed to populate the discord client cache, even if not subscribing to events explicitly.
+    // They do not give permissions to do any operations.
+    // https://discord.com/developers/docs/events/gateway#list-of-intents
+    intents: [
+        // Required to receive responses to vc channel creation and to find vc categories in the channel cache.
+        GatewayIntentBits.Guilds,
+
+        // Required for the bot to know how many members are in a voice channel.
+        GatewayIntentBits.GuildVoiceStates,
+
+        // Privileged Gateway Intents
+        // Privileged Gateway Intents must also be enabled in the discord app bot config:
+        // https://discord.com/developers/applications/1312896264475508839/bot
+        // No Privileged Gateway Intents are required.
+    ],
+});
+
 
 client.on(Events.InteractionCreate, async (interaction: Interaction) => {
     try {
