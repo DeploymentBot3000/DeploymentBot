a19b06a9d00c00d813e2bf37dd1f57e716eb555b
DeploymentBot3000 <195394025+DeploymentBot3000@users.noreply.github.com>
2025-03-14 23:08:59 -0500

    Fix bug in description calendar link

--- src/utils/getGoogleCalendarLink.ts
+++ src/utils/getGoogleCalendarLink.ts
@@ -7,12 +7,18 @@ function formatToGoogleCalendarDate(timestamp: number): string {
     return date.toISOString().replace(/[-:]/g, "").split(".")[0] + "Z";
 }
 
+// encodeURIComponent version that encodes all non alphanumeric characters, including special characters that are valid uri component characters.
+// Since we use links in markdown, we need to escape all characters that might break the markdown syntax like parenthesis.
+function _encodeURIComponent(uriComponent: string) {
+    return encodeURIComponent(uriComponent).replace(/[^A-Za-z0-9]/g, c => "%" + c.charCodeAt(0).toString(16));
+}
+
 export default function getGoogleCalendarLink(title: string, description: string, startDate: number, endDate: number) {
-    const uriTitle = encodeURIComponent(title);
+    const uriTitle = _encodeURIComponent(title);
     // Limit the google description since we need to fit a bunch more details into the same field.
     // 200 is arbitrary here, we know that 500 is too long in some edge cases, likely due to url encoding.
-    const details = encodeURIComponent(description.slice(0, 200));
-    const uriLocation = encodeURIComponent("505th Deployments Channel");
+    const details = _encodeURIComponent(description.slice(0, 200));
+    const uriLocation = _encodeURIComponent("505th Deployments Channel");
     const formattedStart = formatToGoogleCalendarDate(startDate);
     const formattedEnd = formatToGoogleCalendarDate(endDate);
     return `https://www.google.com/calendar/render?action=TEMPLATE&text=${uriTitle}&details=${details}&dates=${formattedStart}/${formattedEnd}&location=${uriLocation}&sf=true&output=xml`;
